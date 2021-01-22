75ceedec5b9f80a83fb7f339286fac5c21e7ac6e
aytug.sevgi <aytug3667@gmail.com>
2021-01-22 15:14:02 +0300

    firebase setup completed

---
+++ lib/view/navigation_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NavigationView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text("Navigation View"),
+      ),
+    );
+  }
+}
